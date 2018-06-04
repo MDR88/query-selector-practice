e9b67c1877858a97d56d64f5edc13ef303b5d77f
mdroberts74 <mikedroberts88@gmail.com>
2018-06-04 10:16:30 -0500

    I created the first line of the js file and I am lost

---
+++ main.js
@@ -0,0 +1,7 @@
+// Use JavaScript to obtain a reference to the first article header and change its text with textContent property to "Welcome the {insert your name here} blog" //
+
+const sectionHead = document.querySelector(".article__header")
+
+
+
+// Use JavaScript to obtain a reference to all article__header elements and change their classList property value to "article__header important".
