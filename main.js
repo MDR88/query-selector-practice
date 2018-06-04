d02e452234ad09fcd8883af65c00fac7ceee2505
mdroberts74 <mikedroberts88@gmail.com>
2018-06-04 10:46:20 -0500

    added to the js file

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 // Use JavaScript to obtain a reference to the first article header and change its text with textContent property to "Welcome the {insert your name here} blog" //
 
-const sectionHead = document.querySelector(".article__header")
-
-
+const sectionHead = document.querySelector(".article__header");
+sectionHead.textContent = "Welcome the {Michael} blog";
 
 // Use JavaScript to obtain a reference to all article__header elements and change their classList property value to "article__header important".
+
+let allHeaders = document.querySelectorAll(".article__header");
+for (let i = 0; i < allHeaders.length; i++) {
+
+}
